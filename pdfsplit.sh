0b8e3e216055599a5c76da6a5eacd35616afb42d
Heymann <lassen@mit.edu>
2019-09-12 06:14:12 +0200

    added pdf splitter

---
+++ pdfsplit.sh
@@ -0,0 +1,42 @@
+# required: sudo apt-get -y install cups-pdf
+
+usage="Usage: $(basename "$0") [-P pages] [PDF-sourcefile] [PDF-destfile]
+	   where pages could be '1,3-5,16' note: based on lpr"
+
+## Check arguments
+if [ "$1" == "-h"  ]; then
+  echo $usage
+  exit 0
+fi
+
+if [ "$#" -lt 2 ]; then
+  echo "Error. expected at least 2 arguments!"
+  echo $usage
+  exit 0
+fi
+
+## assign random integer
+randint=$((1 + RANDOM % 10000))
+
+lpr "$2" -P PDF -o page-ranges="$1" -C "$randint"
+
+# wait till file has been created
+until [ $(ls ~/PDF | grep "$randint" | wc -w) == 1  ]
+do
+     sleep 0.1
+done
+
+## ready paths
+printfile=$(ls ~/PDF | grep "$randint")
+fullfile=${2}
+filename="${fullfile##*/}"
+wd=$(pwd)
+
+## move file
+if [ "$#" == 2 ]; then
+	mv ~/PDF/$printfile $wd/splitted_$filename
+else
+	mv ~/PDF/$printfile $wd/${3}
+fi
+
+
